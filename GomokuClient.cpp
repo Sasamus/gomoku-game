00f1efdc8fd39c582abd024cd4345ae104fa3cf3
Sasamus <labus93@gmail.com>
2014-10-30 01:21:10 +0100

    2014-10-29

--- GomokuClient.cpp
+++ GomokuClient.cpp
@@ -2,7 +2,7 @@
 // Name        : GomokuClient.cpp
 // Author      : Albin Engström
 // Created     : 2014-10-26
-// Modified    : 2014-10-28
+// Modified    : 2014-10-29
 // Description : Implementation of class GomokuClient
 //=============================================================
 #include "GomokuClient.h"
@@ -37,28 +37,38 @@ void GomokuClient::Run()
     char return_message[100];
     std::string send_message;
 
-    //Get input
-    std::cout << "<- ";
-    std::cin >> send_message;
+    while(1<2)
+    {
+        //Get input
+        std::cout << "<- ";
+        std::cin >> send_message;
 
-    //Creates a char array for the message
-    char send_message_char[send_message.size() + 1];
+        //Creates a char array for the message
+        char send_message_char[send_message.size() + 1];
 
-    //Copies the message from send_message to send_message_char
-    std::strcpy(send_message_char, send_message.c_str());
+        //Copies the message from send_message to send_message_char
+        std::strcpy(send_message_char, send_message.c_str());
 
-    //Adds a newline character to send_message_char
-    send_message_char[sizeof(send_message_char) -2] = '\n';
+        //Adds a newline character to send_message_char
+        send_message_char[sizeof(send_message_char) -2] = '\n';
 
-    //Sends the message to a file descriptor
-    write(mp_tcpsocket->get_descriptor(),
-        send_message_char, sizeof(send_message_char));
+        //Sends the message to a file descriptor
+        write(mp_tcpsocket->get_descriptor(),
+            send_message_char, sizeof(send_message_char));
 
+        //Reads the return message from a file descriptor and prints it to screen
+        while(read(mp_tcpsocket->get_descriptor(),
+            return_message, sizeof(return_message)) == 0){}
 
-    //Reads the return message from a file descriptor and prints it to screen
-    while(read(mp_tcpsocket->get_descriptor(),
-        return_message, sizeof(return_message)) == 0){}
         std::cout << "-> " << return_message;
+
+        //Break loop if quiting
+        if(send_message == "QUI")
+        {
+            break;
+        }
+
+    }
 
 
 
